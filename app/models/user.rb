7afe6884c9e8dce850c9da64b7e046624e8c57f0
natekandler <natekandler@gmail.com>
2015-06-09 19:42:36 -0500

    adds base styles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
 include Clearance::User
+
   has_many :requests, foreign_key: :requester_id, class_name: 'Event'
   has_many :responses, foreign_key: :responder_id, class_name: 'Event'
 
