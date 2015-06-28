bc07da5dbedb4c275f5e8488795078fd89ea0d91
natekandler <natekandler@gmail.com>
2015-06-27 20:43:13 -0500

    adds user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,36 @@
 class UsersController < ApplicationController
   def index
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = current_user
+
+    if @user.update(user_params)
+      redirect_to user_path @user
+    else
+      render "profile/edit"
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to sign_in_path
+    else
+      render :new
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit!
+    end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,11 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+ let(:user){FactoryGirl.create(:user)}
+ before(:each){sign_in_as(user)}
+
   context "GET index" do
     it "should be success" do
       get :index
       expect(response).to be_success
     end
   end 
+
+  context "POST update" do
+    it "should update user" do
+      post :update, id: user, user: {email: "new@test.org"}
+      user.reload
+      expect(user.email).to eq "new@test.org"
+    end
+  end
 
 end
