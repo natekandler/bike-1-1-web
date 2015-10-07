1547019d7e1ab0a66fff86ea4b12dba383681b2e
natekandler <natekandler@gmail.com>
2015-10-06 22:28:37 -0500

    adds testing for event actions in api

--- app/api/events/api.rb
+++ app/api/events/api.rb
@@ -11,12 +11,39 @@ module Events
       def authenticate!
         error!('401 Unauthorized', 401) unless current_user
       end
+
+      def api_presenter
+        @api_presenter ||= ApiPresenter.new
+      end
     end
 
-   resource :events do
+    resource :events do
       desc 'Return all events'
       get :index do
-        Event.all.to_json
+        api_presenter.serialized_active_events
+      end
+
+      desc 'Create an event.'
+      params do
+        requires :event, type: Hash do
+          optional :request_txt, type: String, default: "halps me"
+          requires :lat, type: Float, values: -90.0..+90.0
+          requires :lon, type: Float, values: -180.0..+180.0
+          optional :requester_id, type: Integer
+          optional :responder_id, type: Integer
+        end
+      end
+
+      post :create do
+        #authenticate!
+        Event.create!({
+          #requester_id: current_user.id,
+          requester_id: 4,
+          request_text: params.event.request_text,
+          lat: params.event.lat,
+          lon: params.event.lon,
+          active: true
+        })
       end
     end
 
--- app/presenters/event_presenter.rb
+++
@@ -1,5 +0,0 @@
-class EventPresenter
-  def events_serializer events
-    ActiveModel::ArraySerializer.new(events, each_serializer: EventSerializer).to_json
-  end
-end
