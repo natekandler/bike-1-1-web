e0f70b3cfbf0d04eeaf18d1124d2b43d5867875d
natekandler <natekandler@gmail.com>
2015-10-22 20:29:31 -0500

    moves map to render from react component

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -19642,8 +19642,28 @@
 	module.exports = React.createClass({
 	  displayName: 'exports',
 
-	  render: function render() {
+	  componentDidMount: function componentDidMount() {
+	    this.initializeMap();
+	  },
 
+	  initializeMap: function initializeMap() {
+	    var events = this.props.events;
+	    console.warn(events);
+	    var latLng = new google.maps.LatLng(41.930638, -87.713697);
+	    var mapCanvas = document.getElementById('map-canvas');
+	    var mapOptions = {
+	      center: new google.maps.LatLng(41.930619, -87.713654),
+	      zoom: 12
+	    };
+	    var map = new google.maps.Map(mapCanvas, mapOptions);
+	    var marker = new google.maps.Marker({
+	      position: latLng,
+	      map: map,
+	      title: 'title'
+	    });
+	  },
+
+	  render: function render() {
 	    return React.createElement(
 	      'div',
 	      null,
--- webpack/app/components/maps/map.jsx
+++ webpack/app/components/maps/map.jsx
@@ -1,8 +1,29 @@
 var React = require('react');
 
 module.exports = React.createClass({
-  render: function() {
 
+  componentDidMount: function(){
+    this.initializeMap();
+  },
+
+  initializeMap: function() {
+    var events = this.props.events;
+    console.warn(events);
+    var latLng = new google.maps.LatLng(41.930638, -87.713697);
+    var mapCanvas = document.getElementById('map-canvas');
+    var mapOptions = {
+      center: new google.maps.LatLng(41.930619, -87.713654),
+      zoom: 12
+    }
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+    var marker = new google.maps.Marker({
+        position: latLng,
+        map: map,
+        title: 'title'
+    });
+  },
+
+  render: function() {
     return (
       <div>
         <h1>Yo.</h1>
