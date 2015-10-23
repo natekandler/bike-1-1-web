3a55aa9c0deb702858bf1a38c7d21c25b1fcb3a8
natekandler <natekandler@gmail.com>
2015-10-22 21:04:00 -0500

    adds pins dynamically generated from events to map

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -19648,19 +19648,21 @@
 
 	  initializeMap: function initializeMap() {
 	    var events = this.props.events;
-	    console.warn(events);
-	    var latLng = new google.maps.LatLng(41.930638, -87.713697);
 	    var mapCanvas = document.getElementById('map-canvas');
 	    var mapOptions = {
 	      center: new google.maps.LatLng(41.930619, -87.713654),
 	      zoom: 12
 	    };
 	    var map = new google.maps.Map(mapCanvas, mapOptions);
-	    var marker = new google.maps.Marker({
-	      position: latLng,
-	      map: map,
-	      title: 'title'
-	    });
+
+	    for (var event in events) {
+	      var latLng = new google.maps.LatLng(events[event].lat, events[event].lon);
+	      var marker = new google.maps.Marker({
+	        position: latLng,
+	        map: map,
+	        title: events[event].request_text
+	      });
+	    }
 	  },
 
 	  render: function render() {
