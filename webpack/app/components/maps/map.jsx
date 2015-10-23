7c282d49b11782327a9d9b6ea9ad272ad9086f0d
natekandler <natekandler@gmail.com>
2015-10-22 21:03:39 -0500

    adds pins dynamically generated from events to map

--- webpack/app/components/maps/map.jsx
+++ webpack/app/components/maps/map.jsx
@@ -8,19 +8,22 @@ module.exports = React.createClass({
 
   initializeMap: function() {
     var events = this.props.events;
-    console.warn(events);
-    var latLng = new google.maps.LatLng(41.930638, -87.713697);
     var mapCanvas = document.getElementById('map-canvas');
     var mapOptions = {
       center: new google.maps.LatLng(41.930619, -87.713654),
       zoom: 12
     }
     var map = new google.maps.Map(mapCanvas, mapOptions);
-    var marker = new google.maps.Marker({
-        position: latLng,
-        map: map,
-        title: 'title'
-    });
+    
+    for(var event in events){
+      var latLng = new google.maps.LatLng(events[event].lat, events[event].lon);
+      var marker = new google.maps.Marker({
+          position: latLng,
+          map: map,
+          title: events[event].request_text
+      });
+    }
+
   },
 
   render: function() {
